d2bf91fb74e6b68e473f918598cfbf24150191e0
Orosz Ádám <orosz.adam@ckik.loc>
2023-02-13 11:49:46 +0100

    asd

--- program.js
+++ program.js
@@ -1,18 +1,59 @@
-var jatekTer = dosument.getElementId("jatekTer");
+var jatekter = document.getElementById("jatekter");
 
-var baloldal = doocument.createElement("div");
-var kartyadoboz = doocument.createElement("div");
-var pontdoboz = doocument.createElement("div");
-var tabla = doocument.createElement("div");
-var korokdoboz = doocument.createElement("div");
+var leftbox = document.createElement("div")
+var kartyabox = document.createElement("div")
+var pontokbox = document.createElement("div")
+var tabla = document.createElement("div")
+var korokbox = document.createElement("div")
 
-baloldal.appendChild(kartyadoboz);
-baloldal.appendChild(pontdoboz);
-jatekTer.appendChild(baloldal);
-jatekTer.appendChild(tabla);
-jatekTer.appendChild(korokdoboz);
 
-kartyadoboz.innerHTML = "kartyadoboz";
-pontdoboz.innerHTML = "pontdoboz";
-tabla.innerHTML = "tabla";
-korokdoboz = "korokdoboz";
+
+function jatekterbetoltes(){
+    leftbox.appendChild(kartyabox);
+    leftbox.appendChild(pontokbox);
+
+    jatekter.appendChild(leftbox);
+    jatekter.appendChild(tabla);
+    jatekter.appendChild(korokbox);
+
+    kartyabox.innerHTML = "kartyabox";
+    pontokbox.innerHTML = "pontok";
+    tabla.innerHTML = "tabla";
+    korokbox.innerHTML = "korok";
+}
+
+function jatekterelrendezes(){
+    leftbox.id = "balpanel";
+    kartyabox.id="kartyabox";
+    pontokbox.id="pontokbox";
+    tabla.id="tabla";
+    korokbox.id="korokbox"
+}
+
+function tablageneralas(){
+    var k = 1;
+    for (let index = 0; index < 5; index++) {
+        var sorDiv = document.createElement("div");
+        for (let index2 = 0; index2 < 6; index2++) {
+            var oszlopDiv = document.createElement("div");
+            oszlopDiv.classList += " oszlopdiv"
+            sorDiv.appendChild(oszlopDiv);
+            k++;
+        }
+        sorDiv.classList += " sordiv"
+        tabla.appendChild(sorDiv);
+    }
+}
+
+function tablafeltoltes(){
+    
+}
+
+function main(){
+    jatekterbetoltes();
+    jatekterelrendezes();
+    tablageneralas();
+    tablafeltoltes();
+}
+
+main();
